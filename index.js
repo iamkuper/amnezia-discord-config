41b71fdbde73e1b0feb82a04766703c381828772
iamkuper <iamkuper@yandex.ru>
2024-10-08 22:49:51 +0300

    add domains to amnezia lists

--- index.js
+++ index.js
@@ -3,8 +3,29 @@
  */
 
 import fs from 'fs';
+import { promises as dns } from 'dns';
+
 
 const DELIMITER = "\r\n";
+
+
+(async () => {
+
+    // Читаем построчно данные из списка IP и форматируем в массив
+    let config = fs
+        .readFileSync('data/ips.txt')
+        .toString()
+        .split(DELIMITER)
+        .map((hostname) => {
+            return {
+                hostname,
+                ip: ""
+            }
+        })
+
+
+
+})
 
 // Читаем построчно данные из списка IP и форматируем в массив
 const config = fs
@@ -19,8 +40,32 @@ const config = fs
     })
 
 
+const domainsIps = fs.readFileSync('data/domains.txt')
+    .toString()
+    .split(DELIMITER);
+
+
+const dnsConfig = [];
+const dnsPromises = domainsIps.map(async (hostname) => {
+    try {
+        const addresses = await dns.resolve4(hostname);
+        console.log(addresses);
+        addresses.forEach((hostname) => {
+            dnsConfig.push({
+                hostname,
+                ip: ""
+            });
+        });
+    } catch (err) {
+        console.error(`Ошибка при разрешении DNS для ${hostname}:`, err);
+    }
+});
+
+// Ждем завершения всех DNS-запросов
+await Promise.all(dnsPromises);
+
 // Преобразуйте массив в строку JSON
-const jsonConfig = JSON.stringify(config, null, 2);
+const jsonConfig = JSON.stringify([ ...config, ...dnsConfig ], null, 2);
 
 
 fs.writeFile('configs/amnezia.json', jsonConfig, (err) => {
