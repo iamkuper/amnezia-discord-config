86b66e3e8d9dabd17c201757fd082f8c6e26d4fe
iamkuper <iamkuper@gmail.com>
2024-10-08 20:02:01 +0300

    Create repo

---
+++ README.md
@@ -0,0 +1,17 @@
+# Что это и зачем?
+
+Этот репозиторий содержит список IP-адресов, используемых голосовыми серверами Discord, а так же готовый JSON-конфиг для Amnezia VPN. 
+
+
+## Структура репозитория
+
+- `ips.txt` - список всех доменных имен, принадлежащих Discord.
+- `amnezia_sites.json` - готовый конфиг для раздельного туннелирования в Amnezia VPN
+- `index.js` - NodeJS скрипт для генерации конфига из TXT 
+
+
+## Поддержать автора
+
+- USDT (TRC20): TQANwUtCf9UmLZyM3Ty18hDsYpPAVmrpbH
+- BTC : bc1quvv5knvrvya5hn4xzpsfp5tpm5n9yuvmscmg6t
+- ETH : 0xF6a93e81A7cf88b22F5Cec3ABfC0A2295A5Aa7A3
---
+++ index.js
@@ -0,0 +1,29 @@
+/**
+ * Create Amnezia VPN Discord IP list
+ */
+
+import fs from 'fs';
+
+const DELIMITER = "\r\n";
+
+// Читаем построчно данные из списка IP и форматируем в массив
+const config = fs
+    .readFileSync('ips.txt')
+    .toString()
+    .split(DELIMITER)
+    .map((hostname) => {
+        return {
+            hostname,
+            ip: ""
+        }
+    })
+
+
+// Преобразуйте массив в строку JSON
+const jsonConfig = JSON.stringify(config, null, 2);
+
+
+fs.writeFile('amnezia_sites.json', jsonConfig, (err) => {
+    if (err) console.error('Ошибка записи в файл', err);
+    else console.log('Файл успешно сохранен.');
+});
