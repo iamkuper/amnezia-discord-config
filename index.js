dc97c40560b30a22735b55aad0aecc61a98948b8
iamkuper <iamkuper@yandex.ru>
2024-10-08 22:55:19 +0300

    add domains to amnezia lists

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 /**
  * Create Amnezia VPN Discord IP list
  */
-
 import fs from 'fs';
-import { promises as dns } from 'dns';
+import { Resolver } from 'dns/promises'; // Используем promises API
 
-
+// Разделитель строк в TXT
 const DELIMITER = "\r\n";
+const DNS_SERVER = "8.8.8.8";
 
+// Устанавливаем кастомные DNS-серверы
+const resolver = new Resolver();
+resolver.setServers([ DNS_SERVER ]);
 
 (async () => {
 
-    // Читаем построчно данные из списка IP и форматируем в массив
+    // Читаем построчно данные из списка IP и форматируем конфиг
     let config = fs
         .readFileSync('data/ips.txt')
         .toString()
@@ -23,52 +26,39 @@ const DELIMITER = "\r\n";
             }
         })
 
+    // Загружаем домены
+    const domainsIps = fs.readFileSync('data/domains.txt')
+        .toString()
+        .split(DELIMITER);
 
-
-})
-
-// Читаем построчно данные из списка IP и форматируем в массив
-const config = fs
-    .readFileSync('data/ips.txt')
-    .toString()
-    .split(DELIMITER)
-    .map((hostname) => {
-        return {
-            hostname,
-            ip: ""
-        }
-    })
-
-
-const domainsIps = fs.readFileSync('data/domains.txt')
-    .toString()
-    .split(DELIMITER);
-
-
-const dnsConfig = [];
-const dnsPromises = domainsIps.map(async (hostname) => {
-    try {
-        const addresses = await dns.resolve4(hostname);
-        console.log(addresses);
-        addresses.forEach((hostname) => {
-            dnsConfig.push({
-                hostname,
-                ip: ""
+    // Получаем списки IP из DNS
+    const dnsConfig = [];
+    const dnsPromises = domainsIps.map(async (hostname) => {
+        try {
+            const addresses = await resolver.resolve4(hostname);
+            console.log(addresses);
+            addresses.forEach((hostname) => {
+                dnsConfig.push({
+                    hostname,
+                    ip: ""
+                });
             });
-        });
-    } catch (err) {
-        console.error(`Ошибка при разрешении DNS для ${hostname}:`, err);
-    }
-});
+        } catch (err) {
+            console.error(`Ошибка при разрешении DNS для ${hostname}:`, err);
+        }
+    });
 
-// Ждем завершения всех DNS-запросов
-await Promise.all(dnsPromises);
+    // Ждем завершения всех DNS-запросов
+    await Promise.all(dnsPromises);
 
-// Преобразуйте массив в строку JSON
-const jsonConfig = JSON.stringify([ ...config, ...dnsConfig ], null, 2);
+    // Преобразуйте массив в строку JSON
+    const jsonConfig = JSON.stringify([ ...config, ...dnsConfig ], null, 2);
 
 
-fs.writeFile('configs/amnezia.json', jsonConfig, (err) => {
-    if (err) console.error('Ошибка записи в файл', err);
-    else console.log('Файл успешно сохранен.');
-});
+    fs.writeFile('configs/amnezia.json', jsonConfig, (err) => {
+        if (err) console.error('Ошибка записи в файл', err);
+        else console.log('Файл успешно сохранен.');
+    });
+
+
+})();
